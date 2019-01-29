b93d711319368719d068bedefdfe27422c0c1571
perlancar (on pc-office) <perlancar@gmail.com>
2019-01-29 16:26:48 +0700

    Modify SysRun::Alone to Sys::RunAlone::Flexible

--- lib/Sys/RunAlone/Flexible.pm
+++ lib/Sys/RunAlone/Flexible.pm
@@ -1,7 +1,7 @@
-package Sys::RunAlone;
+package Sys::RunAlone::Flexible;
 
-# version info
-$VERSION= '0.13';
+# DATE
+# VERSION
 
 # make sure we're strict and verbose as possible
 use strict;
@@ -14,8 +14,44 @@ use Fcntl ':flock';
 my $silent;
 my $retry;
 
-# satisfy -require-
-1;
+sub lock {
+    no warnings;
+
+    # skipping
+    if ( my $skip= $ENV{SKIP_SYS_RUNALONE} ) {
+        print STDERR "Skipping " . __PACKAGE__ . " check for '$0'\n"
+          if !$silent and $skip > 1;
+    }
+
+    # no data handle, we're screwed
+    elsif ( tell( *main::DATA ) == -1 ) {
+        print STDERR "Add __END__ to end of script '$0'"
+          . " to be able use the features of Sys::RunALone::Flexible\n";
+        exit 2;
+    }
+
+    # we're not alone!
+    elsif ( !flock main::DATA, LOCK_EX | LOCK_NB ) {
+
+        # need to retry
+        if ($retry) {
+            my ( $times, $sleep )= split ',', $retry;
+            $sleep ||= 1;
+            while ( $times-- ) {
+                sleep $sleep;
+
+                # we're alone!
+                goto ALLOK if flock main::DATA, LOCK_EX | LOCK_NB;
+            }
+        }
+
+        # we're done
+        print STDERR "A copy of '$0' is already running\n" if !$silent;
+        exit 1;
+    }
+
+  ALLOK:
+}
 
 #-------------------------------------------------------------------------------
 #
@@ -51,85 +87,71 @@ sub import {
     return;
 } #import
 
-#-------------------------------------------------------------------------------
-
-INIT {
-    no warnings;
-
-    # skipping
-    if ( my $skip= $ENV{SKIP_SYS_RUNALONE} ) {
-        print STDERR "Skipping " . __PACKAGE__ . " check for '$0'\n"
-          if !$silent and $skip > 1;
+{
+    # to shut up the 'Too late to run INIT block' warning
+    no warnings 'void';
+    INIT {
+        lock();
     }
+}
 
-    # no data handle, we're screwed
-    elsif ( tell( *main::DATA ) == -1 ) {
-        print STDERR "Add __END__ to end of script '$0'"
-          . " to be able use the features of Sys::RunALone\n";
-        exit 2;
-    }
+# satisfy -require-
+1;
 
-    # we're not alone!
-    elsif ( !flock main::DATA, LOCK_EX | LOCK_NB ) {
-
-        # need to retry
-        if ($retry) {
-            my ( $times, $sleep )= split ',', $retry;
-            $sleep ||= 1;
-            while ( $times-- ) {
-                sleep $sleep;
-
-                # we're alone!
-                goto ALLOK if flock main::DATA, LOCK_EX | LOCK_NB;
-            }
-        }
-
-        # we're done
-        print STDERR "A copy of '$0' is already running\n" if !$silent;
-        exit 1;
-    }
-
-  ALLOK:
-} #INIT
-
-#-------------------------------------------------------------------------------
+# ABSTRACT: make sure only one invocation of a script is active at a time
 
 __END__
 
-=head1 NAME
-
-Sys::RunAlone - make sure only one invocation of a script is active at a time
-
 =head1 SYNOPSIS
 
- use Sys::RunAlone;
+Use like you would use L<Sys::RunAlone>:
+
+ use Sys::RunAlone::Flexible;
  # code of which there may only be on instance running on system
 
- use Sys::RunAlone silent => 1;
+ use Sys::RunAlone::Flexible silent => 1;
  # be silent if other running instance detected
 
- use Sys::RunAlone retry => 50;
+ use Sys::RunAlone::Flexible retry => 50;
  # retry execution 50 times with wait time of 1 second in between
 
- use Sys::RunAlone retry => '55,60';
+ use Sys::RunAlone::Flexible retry => '55,60';
  # retry execution 55 times with wait time of 60 seconds in between
 
- use Sys::RunAlone 'silent';
+ use Sys::RunAlone::Flexible 'silent';
  # obsolete form of silent => 1
 
+Use in run-time:
+
+ require Sys::RunAlone::Flexible;
+ Sys::RunAlone::Flexible->import(retry => "55,60");
+
+ # then, somewhere in your program
+ sub run {
+     Sys::RunAlone::Flexible::lock();
+ }
+
 =head1 DESCRIPTION
+
+Sys::RunAlone::Flexible is a fork of L<Sys::RunAlone> 0.13. It's just like
+Sys::RunAlone but it can be used at run-time too. The main logic is moved from
+INIT block to the L<lock> subroutine which you can invoke at run-time. But, if
+you "use Sys::RunAlone::Flexible" at compile-time like you would normally use
+Sys::RunAlone, the lock() subroutine will still be invoked at INIT phase.
+
+The rest of the documentation is Sys::RunAlone's.
 
 Provide a simple way to make sure the script from which this module is
 loaded, is only running once on the server.  Optionally allow for retrying
 execution until the other instance of the script has finished.
 
-=head1 VERSION
-
-This documentation describes version 0.12.
-
 =head1 METHODS
 
 There are no methods.
+
+=head1 FUNCTIONS
+
+=head2 lock
 
 =head1 THEORY OF OPERATION
 
@@ -137,8 +159,9 @@ The functionality of this module depends on the availability of the DATA
 handle in the script from which this module is called (more specifically:
 in the "main" namespace).
 
-At INIT time, it is checked whethere there is a DATA handle: if not, it
-exits with an error message on STDERR and an exit value of 2.
+At compile/INIT time (or when you run L</lock>), it is checked when there is a
+DATA handle: if not, it exits with an error message on STDERR and an exit value
+of 2.
 
 If the DATA handle is available, and it cannot be C<flock>ed, it exits
 with an error message on STDERR and an exit value of 1.  The error message
@@ -218,7 +241,7 @@ running (and won't be started by cronjobs while making changes) if you really
 want to be 100% sure that only one instance of the script is running at the
 same time.
 
-=head1 ACKNOWLEDGEMENTS
+=head1 SYS::RUNALONE ACKNOWLEDGEMENTS
 
 Inspired by Randal Schwartz's mention of using the DATA handle as a semaphore
 on the London PM mailing list.
@@ -228,13 +251,13 @@ this module.
 
 =head1 SEE ALSO
 
-L<Sys::RunAlways>.
+L<Sys::RunAlone>.
 
-=head1 AUTHOR
+=head1 SYS::RUNALONE AUTHOR
 
  Elizabeth Mattijsen
 
-=head1 COPYRIGHT
+=head1 SYS::RUNALONE COPYRIGHT
 
 Copyright (c) 2005, 2006, 2008, 2009, 2011, 2012 Elizabeth Mattijsen
 <liz@dijkmat.nl>.  Copyright (c) 2017 Ben Tilly <btilly@gmail.com>.  All
